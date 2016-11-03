196e3a4bf7a20508f59b2f2c06bcbe7d4afbb79e
jesse soldat <jsoldat@hotmail.com>
2016-11-03 15:54:39 +0700

    can view multiple articles

--- app.ts
+++ app.ts
@@ -4,8 +4,29 @@
 import {bootstrap} from "angular2/platform/browser";
 import {Component} from "angular2/core";
 
+class Article {
+	title: string;
+	link: string;
+	votes: number;
+
+	constructor(title: string, link: string, votes?: number){
+		this.title = title;
+		this.link = link;
+		this.votes = votes || 0;
+	}
+
+	voteUp(): void {
+		this.votes += 1;
+	}
+
+	voteDown(): void {
+		this.votes -= 1;
+	}
+}
+
 @Component({
 	selector: 'reddit-article',
+	inputs: ['article']
 	host: {
 		class: 'row'
 	},
@@ -13,7 +34,7 @@ import {Component} from "angular2/core";
     <div class="four wide column center aligned votes">
       <div class="ui statistic">
         <div class="value">
-          {{ votes }}
+          {{ article.votes }}
         </div>
         <div class="label">
           Points
@@ -21,8 +42,8 @@ import {Component} from "angular2/core";
       </div>
     </div>
     <div class="twelve wide column">
-      <a class="ui large header" href="{{ link }}">
-        {{ title }}
+      <a class="ui large header" href="{{ article.link }}">
+        {{ article.title }}
       </a>
       <ul class="ui big horizontal list voters">
         <li class="item">
@@ -43,23 +64,15 @@ import {Component} from "angular2/core";
 
 })
 class ArticleComponent {
-	votes: number;
-	title: string;
-	link: string;
+	article: Article;
 
-	constructor(){
-		this.title = "Angular 2";
-		this.link = "http://angular.io";
-		this.votes = 10;
-	}
-
-	voteUp(){
-		this.votes += 1;
+	voteUp(): boolean{
+		this.article.voteUp();
 		return false;
 	}
 
-	voteDown(){
-		this.votes -= 1;
+	voteDown(): boolean{
+		this.article.voteDown();
 		return false;
 	}
 }
@@ -90,15 +103,22 @@ class ArticleComponent {
 	</form>
 
 	<div class="ui grid posts">
-      <reddit-article>
+      <reddit-article
+      *ngFor="#article of articles"
+			[article]="article">
       </reddit-article>
   </div>
 	`
 })
 class RedditApp {
-	
+	articles: Article[];
 
 	constructor(){
+		this.articles = [
+			new Article('Angular 2', 'http://angular.io', 3),
+			new Article('Fullstack', 'http://fullstack.io', 2),
+			new Article('Angular Homepage', 'http://angular.io', 1),
+		];
 		
 	}
 	addArticle(title: HTMLInputElement, link: HTMLInputElement): void {
